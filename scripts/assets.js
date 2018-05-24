53debb14476e74059ca4bdbebcf5d2947e0da1b3
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-05-24 12:09:29 -0400

    create cases for different form types

--- scripts/assets.js
+++ scripts/assets.js
@@ -1,7 +1,6 @@
 var fs = require('fs-extra');
 var sass = require('node-sass');
 var handlebars = require('handlebars');
-var glob = require('glob-fs')({ gitignore: true });
 
 var fields = require('../src/fields.json');
 var config = require('../scripts/config.json');
@@ -21,7 +20,7 @@ module.exports = {
 
     html: function(path, absolutePath, version) {
         fs.mkdirsSync(path + 'tools/' + config.name + '');
-        fs.writeFileSync(path + 'tools/' + config.name + '/index.html', this.compileHtml('tool'));
+        fs.writeFileSync(path + 'tools/' + config.name + '/index.html', this.compileHtml('tool', absolutePath));
 
         fs.mkdirsSync(path + 'embed/' + config.name + '/v' + version)
         fs.writeFileSync(path + 'embed/' + config.name + '/v' + version + '/index.html',
@@ -31,25 +30,32 @@ module.exports = {
         console.log('updated html!');
     },
 
-    compileHtml: function(file) {
+    compileHtml: function(file, absolutePath) {
+        handlebars.registerHelper("switch", function(value, options) {
+            this._switch_value_ = value;
+            var html = options.fn(this); // Process the body of the switch block
+            delete this._switch_value_;
+            return html;
+        });
+
+        handlebars.registerHelper("case", function(value, options) {
+            if (value == this._switch_value_) {
+                return options.fn(this);
+            }
+        });
+
+        handlebars.registerHelper('handlise', function(string) {
+            return string.toLowerCase().replace(/ /g, '-').replace(/\//g, '');
+        })
+
         var html = fs.readFileSync('src/' + file + '/index.html', 'utf8');
         var template = handlebars.compile(html);
         var data = {
             'name': config.name,
-            'version': config.version,
+            'version': 'v' + config.version,
+            'path': absolutePath,
             'fields': fields
         };
-
-        var partials = glob.readdirSync('src/' + file + '/**/*.html');
-
-        console.log(partials);
-
-        partials.forEach(function(partial) {
-            var name = partial.replace('src/templates/', '').split('.')[0];
-            var template = fs.readFileSync(partial, 'utf8');
-
-            handlebars.registerPartial(name, template);
-        });
 
         return template(data);
     }
