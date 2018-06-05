3f742acef3b746a3ede02fffe52cae1ae7873159
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-06-05 17:15:07 -0400

    on json change rebuild

--- scripts/local.js
+++ scripts/local.js
@@ -8,7 +8,7 @@ var config = require('../scripts/config.json');
 watch('src', function(file) {
     var fileExt = file.substring(file.lastIndexOf('.') + 1);
 
-    if (fileExt === 'html' || fileExt === 'svg') {
+    if (fileExt === 'html' || fileExt === 'svg' || fileExt === 'json') {
         console.log('updating html');
         cmd.get('npm run compile -- local preview html', function(data) { console.log(data); });
     } else if (fileExt === 'scss') {
